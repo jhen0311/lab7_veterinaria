5a71affbf8d11de67010ca1602f443d1da0b99d2
jhen0311 <a20206073@pucp.edu.pe>
2026-06-12 17:41:58 -0500

    Update BorrarMascotaServlet.java

--- LAB_Veterinaria/src/main/java/com/veterinaria/servlet/BorrarMascotaServlet.java
+++ LAB_Veterinaria/src/main/java/com/veterinaria/servlet/BorrarMascotaServlet.java
@@ -1,13 +1,11 @@
 package com.veterinaria.servlet;
 
 import com.veterinaria.dao.MascotaDao;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet encargado de eliminar una mascota según su id.
