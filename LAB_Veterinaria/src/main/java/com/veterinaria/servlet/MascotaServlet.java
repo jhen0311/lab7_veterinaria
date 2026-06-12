9ff8d74f6a3254b763071fd19f23e0da8738adbb
jhen0311 <a20206073@pucp.edu.pe>
2026-06-12 17:42:14 -0500

    Update MascotaServlet.java

--- LAB_Veterinaria/src/main/java/com/veterinaria/servlet/MascotaServlet.java
+++ LAB_Veterinaria/src/main/java/com/veterinaria/servlet/MascotaServlet.java
@@ -4,13 +4,11 @@ import com.veterinaria.dao.MascotaDao;
 import com.veterinaria.modelo.Especie;
 import com.veterinaria.modelo.Mascota;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet encargado de listar las mascotas registradas.
